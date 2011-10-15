f0fff865aa8089f2924485ef0914dc9b9fac6f59
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-10-15 13:09:10 -0300

    Updated @santagada setup.sh to remove the gitst2 submodule

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ ln -s User/Sniptastic ..
 ln -s User/SublimeLinter ..
 ln -s User/SublimeCodeIntel ..
 ln -s User/Djaneiro ..
-ln -s User/gitst2 ..
 ln -s User/"Theme - Soda" ..
 ln -s User/jinja2-tmbundle ../Jinja2
 ln -s User/sublime-github ..
